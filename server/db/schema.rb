a8b7ec2502a2b26dc922cbd36dd5fba25fc290ec
Tony Mai <thetonymai@gmail.com>
2015-04-11 19:00:06 -0700

    add vote counts migration and view

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410192050) do
+ActiveRecord::Schema.define(version: 20150412015155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,15 +20,17 @@ ActiveRecord::Schema.define(version: 20150410192050) do
     t.string   "title"
     t.text     "content"
     t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "vote_count",  default: 0
   end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "vote_count", default: 0
   end
 
 end
