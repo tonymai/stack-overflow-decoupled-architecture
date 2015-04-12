6c87133e58335a727f8ce5d61fc0cc4e7909d7da
Tony Mai <thetonymai@gmail.com>
2015-04-11 17:46:51 -0700

    markdown preview widget

--- client/app/assets/javascripts/add-post.js
+++ client/app/assets/javascripts/add-post.js
@@ -3,15 +3,20 @@ $(document).on('page:change', function(){
     e.preventDefault();
     var $title = $('input[name="post[title]"]')
     var $content = $('textarea[name="post[content]"]')
-    
+
+    var $previewTitle = $('.preview-title')
+    var $previewContent = $('.preview-content')
+
     $.ajax({
       url: 'http://localhost:3001' + $('.new-post').attr('action'),
       type: 'POST',
-      data: { question: { title: $title.val(), content: $content.val() }, answer: { title: $title.val(), content: $content.val() } }
+      data: { question: { title: $title.val(), content: $previewContent.html() }, answer: { title: $title.val(), content: $previewContent.html() } }
     }).done(function(response) {
       appendPost([response]);
       $title.val('');
       $content.val('');
+      $previewTitle.html('');
+      $previewContent.html('');
     });
   });
 });
---
+++ client/app/assets/javascripts/preview.js
@@ -0,0 +1,47 @@
+$(document).on('page:change', function(){
+
+  $('input[name="post[title]"]').on('keyup',function(e) {
+    previewTitle();
+  });
+
+  $('textarea[name="post[content]"]').on('keyup',function(e) {
+    previewContent();
+  });
+
+});
+
+
+function previewTitle() {
+  var title = $('input[name="post[title]"]').val();
+  $('.preview-title').html('<h3>'+title+'</h3>');
+};
+
+function previewContent() {
+  var content = $('textarea[name="post[content]"]').val();
+  var converted_content = convertText(content);
+  $('.preview-content').html(converted_content);
+};
+
+function convertText(content) {
+  //line break
+  content = content.replace(/\n/g, '<br />');
+  //bold
+  while (content.match(/\*\*/g) && content.match(/\*\*/g).length >= 2) {
+    content = content.replace(/\*\*/, '<strong>');
+    content = content.replace(/\*\*/, '</strong>');
+  };
+  while (content.match(/\_\_/g) && content.match(/\_\_/g).length >= 2) {
+    content = content.replace(/\_\_/, '<strong>');
+    content = content.replace(/\_\_/, '</strong>');
+  };
+  //italic
+  while (content.match(/\*/g) && content.match(/\*/g).length >= 2) {
+    content = content.replace(/\*/, '<em>');
+    content = content.replace(/\*/, '</em>');
+  };
+  while (content.match(/\_/g) && content.match(/\_/g).length >= 2) {
+    content = content.replace(/\_/, '<em>');
+    content = content.replace(/\_/, '</em>');
+  };
+  return content;
+}
