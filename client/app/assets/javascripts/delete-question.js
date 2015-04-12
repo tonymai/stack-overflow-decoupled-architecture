a67e9e20e31a4f0b3100d9d32e3117af0ec4acf0
Tony Mai <thetonymai@gmail.com>
2015-04-11 18:11:20 -0700

    delete preventdefault not working

--- client/app/assets/javascripts/delete-question.js
+++ client/app/assets/javascripts/delete-question.js
@@ -1,12 +1,13 @@
 $(document).on('page:change', function(){
   $('.delete').on('click', function(e) {
-    e.preventDefault
-    $.ajax({
-      url: 'http://localhost:3001' + $('.delete').attr('href'),
-      type: 'DELETE'
-    }).done(function() {
-      window.location.href = '/';
-    });
+    e.preventDefault;
+    console.log('test')
+    // $.ajax({
+    //   url: 'http://localhost:3001' + $('.delete').attr('href'),
+    //   type: 'DELETE'
+    // }).done(function() {
+    //   window.location.href = '/';
+    // });
   });
   
 });
